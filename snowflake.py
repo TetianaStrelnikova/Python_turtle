66c9fb506d013d242f15283e9b8bb74e223952e6
Tetiana <107929120+TetianaStrelnikova@users.noreply.github.com>
2022-11-25 17:35:32 +0400

    change the name of my polygon

---
+++ snowflake.py
@@ -0,0 +1,47 @@
+import turtle
+turt = turtle.Turtle()
+
+def koch(l):
+  if l >3 :
+    koch(l/3)
+    turt.left(60)
+    koch(l/3)
+    turt.right(120)
+    koch(l/3)
+    turt.left(60)
+    koch(l/3)
+  else:
+    turt.forward(l)
+    
+koch(300)import turtle
+turt = turtle.Turtle()
+turt.speed(5000)
+turtle.colormode(255)
+import random
+def koch(l):
+  if l >3 :
+    koch(l/3)
+    turt.left(60)
+    koch(l/3)
+    turt.right(120)
+    koch(l/3)
+    turt.left(60)
+    koch(l/3)
+  else:
+    turt.forward(l)
+    
+def snowflake(l):
+  for _ in range (4):
+    koch(l)
+    turt.right(90)
+for i in range(50):
+  r = random.randint(0,255)
+  g = random.randint(0,255)
+  b = random.randint(0,255)
+  turt.color((r,g,b))
+  turt.fillcolor((r,g,b))
+  turt.penup()
+  turt.goto(random.randint(-300,300), random.randint(-300,300))
+  turt.pendown()
+  snowflake(random.randint(30,100))
+
